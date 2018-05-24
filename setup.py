4213acb0328217a06822e891cc71801a1b5095d1
Thyago Stall <thstall@gmail.com>
2018-05-24 08:15:49 -0300

    Adds setup for packaging library

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='dotplus',
+    packages=['dotplus'],
+    version='v0.1',
+    description='Client for time cards APIs',
+    author='Thyago Stall',
+    author_email='thstall@gmail.com',
+    url='https://github.com/thyagostall/dotplus',
+    download_url='https://github.com/thyagostall/dotplus/archive/v0.1.tar.gz',
+    keywords=[],
+    classifiers=[],
+)
