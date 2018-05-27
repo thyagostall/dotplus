7cdbfddd898ad2924c98e4bffa71d5f66fad0ce6
Thyago Stall <thstall@gmail.com>
2018-05-26 22:16:17 -0300

    Bump up package version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='dotplus',
     packages=['dotplus'],
-    version='v0.1',
+    version='v0.2',
     description='Client for time cards APIs',
     author='Thyago Stall',
     author_email='thstall@gmail.com',
     url='https://github.com/thyagostall/dotplus',
-    download_url='https://github.com/thyagostall/dotplus/archive/v0.1.tar.gz',
+    download_url='https://github.com/thyagostall/dotplus/archive/v0.2.tar.gz',
     keywords=[],
     classifiers=[],
     install_requires=[
